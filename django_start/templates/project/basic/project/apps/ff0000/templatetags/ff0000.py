54416ff3a3c5f10edace4b18708009c00b6846e0
Taka Kojima <taka@gigafied.com>
2011-11-08 15:20:01 -0800

    added verbatim tag to basic

---
+++ django_start/templates/project/basic/project/apps/ff0000/templatetags/ff0000.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
